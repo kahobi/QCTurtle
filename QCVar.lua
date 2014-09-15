330a574d7e0427c823251c56bcc7308504a54967
kahobi <kahobi@gmail.com>
2014-09-14 23:20:32 -0700

    Create QCVar.lua

---
+++ QCVar.lua
@@ -0,0 +1,29 @@
+NumSect = 0				--The number of sections to be built, colum by colum
+SectDim = 6				--7th segment in a section is shared, the start of the next.
+
+						--Inventory
+NetherQ = 16
+SmokedQ = 15
+NetherF = 14
+SmokedF = 13
+IOMECon = 12
+IOEnCon = 11
+--
+
+--
+--rint("Please specify the number of sections to be built. Recommended not to exceeed 10. "local NumSect = ..."")
+
+--print("Please specify if ME Conduit is to to be added, true/false "local MENCond = ..."")
+--	if MENCond ~= true or MENCond ~= false then
+--		repeat
+--			print("Please specify true or false "local MENCond = ..."")
+--		until(MENConD == true or MENCond == false)
+--	end
+
+--print("Add Energy Conduit to Hall? true/false "local EneCond = ..."")
+--	if EneCond ~= true or EneCond ~= false then
+--		repeat
+--			print("Please specify true or false "local EneCond = ..."")
+--		until(EneCond == true or EneCond == false)
+--	end
+
