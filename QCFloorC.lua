803e61f40f33eaec0f4271d2f09d8fb56c3fd933
kahobi <kahobi@gmail.com>
2014-09-16 13:22:43 -0700

    Update QCFloorC.lua

--- QCFloorC.lua
+++ QCFloorC.lua
@@ -9,123 +9,62 @@ turtle.forward()
 turtle.forward()
 turtle.turnRight()
 
-if (NumSect >= 2) then
-	turtle.select(SmokedQ)
-	for i = 1 , (NumSect-1) do						--Colum 2 - Left smoked, Send
-		for i = 1 , SectDim do
-			turtle.forward()
-			turtle.placeDown()
-		end
-	end
-
-	turtle.forward()
-	turtle.turnRight()
-	turtle.forward()
-	turtle.turnRight()
-	turtle.select(NetherQ)
-	for i =1 , NumSect do							--Colum 3, - Left Nether, Return
-		for i = 1 , SectDim do
-			turtle.forward()
-			turtle.placeDown()
-		end
-	end
-
-	turtle.forward()
-	turtle.turnLeft()
-	turtle.forward()
-	turtle.turnLeft()
-	for i = 1 , NumSect do 							--Colum 4, - Centre Nether --Send
-		for i = 1 , SectDim do
-			turtle.select(NetherF)
-			turtle.forward()
-			turtle.placeDown()
-			if MENetwork then
-				turtle.select(IOMECon)
-				turtle.placeDown()
-			end
-			if EnNetwork then
-				turtle.select(IOEnCon)
-				turtle.placeDown()
-			end
-		end
-	end
-
-	turtle.forward()
-	turtle.turnRight()
-	turtle.forward()
-	turtle.turnRight()
-	for i = 1 , NumSect do							--Colum 5, - Right Nether --Return
-		for i = 1 , SectDim do
-			turtle.forward()
-			turtle.placeDown()
-		end
-	end
-
-	turtle.forward()
-	turtle.turnLeft()
-	turtle.forward()
-	turtle.turnLeft()
-	turtle.select(SmokedQ)
-	for i = 1 , NumSect do							--Colum 6, - Right Smoked --Send
-		for i = 1 , SectDim do
-			turtle.forward()
-			turtle.placeDown()
-		end
-	end
-
-	turtle.turnLeft()								--Return to start
-	turtle.forward()
-	turtle.forward()
-	turtle.turnRight()
-end
---
---
---
---
---
---
 turtle.select(SmokedQ)
-for i = 1 , SectDim do 								--Colum 2, Left Smoked, Send
-	turtle.forward()
-	turtle.placeDown()
+for i = 1 , NumSect do						--Colum 2 - Left smoked, Send
+	for i = 1 , SectDim do
+		turtle.forward()
+		turtle.placeDown()
+	end
 end
+turtle.forward()
+turtle.turnRight()
+turtle.forward()
+turtle.turnRight()
 
-turtle.forward()
-turtle.turnRight()
-turtle.forward()
-turtle.turnRight()
+
 turtle.select(NetherQ)
-for i = 1 , SectDim do								--Colum 3, - Left Nether, Return
-	turtle.forward()
-	turtle.placeDown()
+for i =1 , NumSect do							--Colum 3, - Left Nether, Return
+	for i = 1 , SectDim do
+		turtle.forward()
+		turtle.placeDown()
+	end
 end
-
-
 turtle.forward()
 turtle.turnLeft()
 turtle.forward()
 turtle.turnLeft()
-for i = 1 , SectDim do								--Colum 4, - Centre Nether --Send
-	turtle.select(NetherF)
-	turtle.forward()
-	turtle.placeDown()
-	if MENetwork then
-		turtle.select(IOMECon)
+
+
+for i = 1 , NumSect do 						--Colum 4, - Centre Nether --Send
+	for i = 1 , SectDim do
+		turtle.select(NetherF)
+		turtle.forward()
 		turtle.placeDown()
-	end
-	if EnNetwork then
-		turtle.select(IOEnCon)
-		turtle.placeDown()
+		if MENetwork then
+			turtle.select(IOMECon)
+			turtle.placeDown()
+		end
+		if EnNetwork then
+			turtle.select(IOEnCon)
+			turtle.placeDown()
+		end
 	end
 end
-
-
 turtle.forward()
 turtle.turnRight()
 turtle.forward()
 turtle.turnRight()
 
-turtle.select(NetherF)								--Colum 5, - Right Nether --Return
+turtle.select(NetherQ)
+if (NumSect > = 2) then
+	for i = 1 , (NumSect - 1) do						--Colum 5, - Right Nether --Return
+		for i = 1 , SectDim do
+			turtle.forward()
+			turtle.placeDown()
+		end
+	end
+end
+turtle.select(NetherF)
 turtle.forward()
 turtle.placeDown()
 if MENetwork then
@@ -141,14 +80,21 @@ for i = 1 , (SectDim-1)  do
 	turtle.forward()
 	turtle.placeDown()
 end
-
-
 turtle.forward()
 turtle.turnLeft()
 turtle.forward()
 turtle.turnLeft()
+
 turtle.select(SmokedQ)
-for i = 1 , (SectDim-1) do							--Colum 6, - Right Smoked --Send
+if (NumSect > = 2) then
+	for i = 1 , (NumSect - 1) do								--Colum 6, - Right Smoked --Send
+		for i = 1 , SectDim do
+			turtle.forward()
+			turtle.placeDown()
+		end
+	end
+end
+for i = 1 , (SectDim-1) do
 	turtle.forward()
 	turtle.placeDown()
 end
@@ -163,13 +109,11 @@ turtle.placeDown()
 		turtle.select(IOEnCon)
 		turtle.placeDown()
 	end
-turtle.select(SmokedQ)
-
-
-turtle.turnLeft()										--Return to start
+turtle.turnLeft()												--Return to start
 turtle.forward()
 turtle.forward()
 turtle.turnRight()
+
 for i = 1 , NumSect do
 	for i = 1 , SectDim do
 	turtle.back()
